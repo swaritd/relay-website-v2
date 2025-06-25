09e69e1f236f937c98b1832695fd58767311e6bf
Swarit Dholakia <dholakia.swarit@gmail.com>
2025-06-25 09:22:46 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Relay',
+  description: 'Never Miss Another Patient Call',
   generator: 'v0.dev',
 }
 
